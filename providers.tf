cadb09f2015b9d01a2af8bc9b2ee71448dfb49e4
Davi Oliveira <158470843+daviolivas@users.noreply.github.com>
2025-05-17 19:01:30 -0300

    Ajustando secrets e variáveis no código

--- providers.tf
+++ providers.tf
@@ -16,8 +16,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = ""
-  client_secret   = ""
-  tenant_id       = ""
-  subscription_id = ""
+  client_id       = var.arm_client_id
+  client_secret   = var.arm_client_secret
+  tenant_id       = var.arm_tenant_id
+  subscription_id = var.arm_subscription_id
 }
