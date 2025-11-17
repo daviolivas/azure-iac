57cb92d4d9080d85ed83e9e658574f6447bd51c5
Davi Oliveira <158470843+daviolivas@users.noreply.github.com>
2025-11-17 01:38:11 -0300

    vars

--- variables.tf
+++ variables.tf
@@ -9,3 +9,31 @@ variable "machine_size" {
 variable "class_c_subnet_id" {
   default     = "/subscriptions/c4c24a8e-87b6-4192-a023-d73a898fca7e/resourceGroups/networks/providers/Microsoft.Network/virtualNetworks/vn-sandbox/subnets/first-subnet"
 }
+
+# Variável para o Client ID (Application ID)
+variable "arm_client_id" {
+  description = "The Azure Client ID (Application ID) for the Service Principal."
+  type        = string
+  sensitive   = true # Recomendado para não mostrar o valor no output do plan
+}
+
+# Variável para o Client Secret (Chave/Senha)
+variable "arm_client_secret" {
+  description = "The Azure Client Secret (password) for the Service Principal."
+  type        = string
+  sensitive   = true
+}
+
+# Variável para o Tenant ID (Directory ID)
+variable "arm_tenant_id" {
+  description = "The Azure Tenant ID (Directory ID)."
+  type        = string
+  sensitive   = true
+}
+
+# Variável para o Subscription ID
+variable "arm_subscription_id" {
+  description = "The Azure Subscription ID."
+  type        = string
+  sensitive   = true
+}
